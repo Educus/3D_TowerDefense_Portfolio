b1e2baba033e1e85f3ae8d4b3e1462da5bf5a4c7
Educus <kms369400@naver.com>
2024-06-21 03:07:27 +0900

    24.06.20 - 2 몬스터 위치 이동, 스폰매니저

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+
+    Queue<Transform> cornerQueue;
+
+    public void Setup(Transform[] corrners)
+    {
+        cornerQueue = new Queue<Transform>(corrners);
+        transform.position = cornerQueue.Peek().position;
+    }
+
+    private void Update()
+    {
+        if (cornerQueue.Count <= 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 destination = cornerQueue.Peek().position;
+        if (Vector3.Distance(transform.position, destination) <= 0)
+            cornerQueue.Dequeue();
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
+            // transform.LookAt(destination);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(destination - transform.position), 0.1f);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] Enemy[] prefab;
+    [SerializeField] int enemyValue;
+    [SerializeField] float spawnRate;
+    [SerializeField] int spawnCount;
+    [SerializeField] Transform[] corners;
+
+    private void Start()
+    {
+        StartCoroutine(IESpawn());
+    }
+
+    public IEnumerator IESpawn()
+    {
+        int remaining = spawnCount;
+        while (remaining > 0)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            Enemy newEnemy = Instantiate(prefab[enemyValue], transform);
+            newEnemy.Setup(corners);
+            remaining -= 1;
+        }
+    }
+
+}
